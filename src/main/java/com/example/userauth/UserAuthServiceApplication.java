1ed8d79bafb02b5129b60e65424db71ebc310264
Rahul Charvekar <rahulcharvekar@gmail.com>
2025-10-16 15:05:02 +0530

    more generic code

--- src/main/java/com/example/userauth/UserAuthServiceApplication.java
+++ src/main/java/com/example/userauth/UserAuthServiceApplication.java
@@ -3,7 +3,7 @@ package com.example.userauth;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"com.example.userauth", "com.shared"})
+@SpringBootApplication
 public class UserAuthServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserAuthServiceApplication.class, args);
